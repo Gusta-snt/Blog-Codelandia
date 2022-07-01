96717e9c99706e549c97b5eee061ec41409c040d
Gustavo Ferreira <gustavofrsnt@gmail.com>
2022-07-01 18:51:21 -0300

    :white_check_mark: Add DocumentMain component.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import "./src/global/reset.css"
 import "./src/global/colors.css"
-import CardArticle from "./src/components/CardArticle";
+import DocumentHeader from "./src/components/DocumentHeader";
+import DocumentMain from "./src/components/DocumentMain";
 
-const $htmlCardArticle = CardArticle("Tittle", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, doloribus ipsa provident nobis, animi quos illum illo optio eius, molestias tempore incidunt expedita dolor dignissimos! Nesciunt ipsa eaque deleniti cumque!")
+const $htmlDocumentMain = DocumentMain()
+const $htmlDocumentHeader = DocumentHeader();
 const $app = document.getElementById("app")
 
-$app.insertAdjacentHTML("beforeend",$htmlCardArticle);
+$app.insertAdjacentHTML("beforeend",$htmlDocumentHeader + $htmlDocumentMain);
---
+++ src/components/DocumentMain/index.js
@@ -0,0 +1,21 @@
+import CardArticle from '../CardArticle';
+import './style.css'
+
+function DocumentMain() {
+    return `
+    <main>
+        <div class="main-wrapper">
+
+            ${CardArticle("Agora é oficial: o Windows 11 está vindo", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, doloribus ipsa provident nobis, animi quos illum illo optio eius, molestias tempore incidunt expedita dolor dignissimos! Nesciunt ipsa eaque deleniti cumque!")}
+
+            ${CardArticle("Tim Berners-Lee vai leiloar código-fonte da web", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident impedit ducimus odit assumenda molestias aut consequuntur mollitia laborum sequi. Voluptatem fuga quibusdam perferendis, cupiditate molestias natus dignissimos? Fugiat, dolorem animi.")}
+
+            ${CardArticle("Tem Firefox novo no pedaço e você vai querer migrar", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptas, doloremque animi quam corporis voluptatibus officia sed eligendi reiciendis deleniti voluptates dolore omnis distinctio libero. Deserunt doloribus placeat tenetur minima.")}
+
+            ${CardArticle("John McAfee, criador do antivírus McAfee, morre", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque odio laboriosam id quaerat qui similique cum quisquam eos officia sequi et dolorum soluta, molestiae ipsa rerum dolore recusandae vero sit!")}
+            
+        </div>
+    </main>`
+}
+
+export default DocumentMain;
